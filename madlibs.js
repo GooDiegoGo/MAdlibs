f88ed8de9da7e6eaf3ec3e225b3be0b5b1ed6a54
tezu <tezu@fresnou.org>
2023-04-12 10:27:17 -0700

    Madlibs

---
+++ madlibs.js
@@ -0,0 +1,35 @@
+const readline = require ('readline-sync');
+console.log()
+readline.question ("Adjective" );
+console.log()
+readline.question ("Adjective" );
+
+readline.question ("Celebrity" );
+
+readline.question ("Celebrity" );
+
+readline.question ("Animal" );
+
+readline.question ("Verb" );
+
+readline.question ("Noun" );
+
+readline.question ("Noun" );
+
+readline.question ("part of body" );
+
+readline.question ("Adverb" );
+
+readline.question ("Adjective" );
+
+readline.question ("Adjective" );
+
+readline.question ("Adjective" );
+
+readline.question ("Noun" );
+
+readline.question ("Noun" );
+
+readline.question ("person in room(male) " );
+
+
